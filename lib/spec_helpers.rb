1e7a6b4e53293bfd66c7bf6ebbe5b2606adb127c
Cory <cory.forsyth@gmail.com>
2009-03-07 14:39:58 -0500

    moved spec helpers to correct location

---
+++ lib/spec_helpers.rb
@@ -0,0 +1,32 @@
+# Matcher to test equality between images
+class EqualImage
+  def initialize(other_image)
+    @other_image = load_image(other_image)
+  end
+  
+  def load_image(image)
+    case image
+    when String
+      Magick::ImageList.new(image)
+    else
+      image
+    end
+  end
+  
+  def matches?(image)
+    @image = load_image(image)
+    return (@other_image <=> @image) == 0
+  end
+  
+  def failure_message
+    "expected the images to be equal"
+  end
+  
+  def negative_failure_message
+    "images should not have been equal but they were"
+  end
+end
+
+def equal_image(other_image)
+  EqualImage.new(other_image)
+end
