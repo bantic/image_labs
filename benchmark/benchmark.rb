04e5af8f7618253bfc4b7041de33ef6221af1778
Cory <cory.forsyth@gmail.com>
2009-03-09 22:32:35 -0400

    Updated benchmarks

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/../lib/lib"
 require 'benchmark'
 
-img = img(File.dirname(__FILE__) + "/../fixtures/edge_input.jpg")
+img = img(File.dirname(__FILE__) + "/../fixtures/cart_gray.jpg")
  
 n = 10
 Benchmark.bm do |x|
@@ -9,3 +9,11 @@ Benchmark.bm do |x|
   x.report("two_d_array_with_export_pixels") { n.times do; ar = img.two_d_array_with_export_pixels; end }
   x.report("two_d_array_with_dispatch") { n.times do; ar = img.two_d_array_with_dispatch; end }
 end
+
+# Results
+#
+#       user     system      total        real
+# two_d_array_with_view  4.050000   1.110000   5.160000 (  5.199948)
+# two_d_array_with_export_pixels  0.510000   0.240000   0.750000 (  0.760364)
+# two_d_array_with_dispatch  0.600000   0.240000   0.840000 (  0.849024)
+#
