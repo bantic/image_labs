7cbc809bbcf835a2dc5a9474458aec788dace988
Cory <cory.forsyth@gmail.com>
2009-03-11 22:32:24 -0400

    Added welcome message to rmagick shell

---
+++ lib/shell_lib.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/lib.rb'
+puts <<END
+== RMagick Shell ==
+#{Long_version}
+===================
+END
+
+if Long_version =~ /Q16/
+  puts <<-END
+    [Warning] ImageMagick is compiled with Quantum Depth of 16, which may make
+              some functions very slow.
+  END
+end
