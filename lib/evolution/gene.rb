123987a6efbc7bfa1a8d6ebd29fb076543c965ce
Cory <cory.forsyth@gmail.com>
2009-03-12 21:57:03 -0400

    simplified/clarified Gene

--- lib/evolution/gene.rb
+++ lib/evolution/gene.rb
@@ -1,6 +1,7 @@
 class Gene
   MAX_RGB   = 255
   MAX_ALPHA = 1.0
+  MAX_POS   = 200
   MAX_DIM   = 200
 
   DEFAULT_MUTATION_RATE = 0.2
@@ -8,35 +9,35 @@ class Gene
   ATTRS = [:r, :g, :b, :a, :x, :y, :w, :h]
 
   def initialize
-    @hash = {}
+    @genotype = {}
     ATTRS.each do |key|
-      @hash[key] = rand
+      @genotype[key] = rand
     end
   end
   
   def phenotype
-    [
-     (@hash[:r] * MAX_RGB).to_i,
-     (@hash[:g] * MAX_RGB).to_i,
-     (@hash[:b] * MAX_RGB).to_i,
-     (@hash[:a] * MAX_ALPHA).to_f,
-     (@hash[:x] * MAX_DIM).to_i,
-     (@hash[:y] * MAX_DIM).to_i,
-     (@hash[:w] * MAX_DIM).to_i,
-     (@hash[:h] * MAX_DIM).to_i
-    ]
+    {
+     :r => (@genotype[:r] * MAX_RGB).to_i,
+     :g => (@genotype[:g] * MAX_RGB).to_i,
+     :b => (@genotype[:b] * MAX_RGB).to_i,
+     :a => (@genotype[:a] * MAX_ALPHA).to_f,
+     :x => (@genotype[:x] * MAX_POS).to_i,
+     :y => (@genotype[:y] * MAX_POS).to_i,
+     :w => (@genotype[:w] * MAX_DIM).to_i,
+     :h => (@genotype[:h] * MAX_DIM).to_i
+    }
   end
   
   def magick_draw_obj
     pheno = phenotype
     d = Draw.new
-    d.fill("rgba(#{pheno[0]},#{pheno[1]},#{pheno[2]},#{pheno[3]}")
-    d.ellipse(pheno[4],pheno[5],pheno[6],pheno[7],0,360)
+    d.fill("rgba(#{pheno[:r]},#{pheno[:g]},#{pheno[:b]},#{pheno[:a]}")
+    d.ellipse(pheno[:x],pheno[:y],pheno[:w],pheno[:h],0,360)
   end
   
   def mutate!(mutation_rate=DEFAULT_MUTATION_RATE)
-    @hash.each_key do |attribute|
-      @hash[attribute] = rand if rand < DEFAULT_MUTATION_RATE
+    @genotype.each_key do |attribute|
+      @genotype[attribute] = rand if rand < DEFAULT_MUTATION_RATE
     end
   end
 end
