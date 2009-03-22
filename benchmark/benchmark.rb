b957a9239c4f02a9cd9fc72c9a69ecf3d4801adf
Cory <cory.forsyth@gmail.com>
2009-03-22 17:41:30 -0400

    removed benchmark export_pixels_as_str

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -8,6 +8,7 @@ Benchmark.bm do |x|
   x.report("two_d_array_with_view") { n.times do; ar = img.two_d_array_with_view; end }
   x.report("two_d_array_with_export_pixels") { n.times do; ar = img.two_d_array_with_export_pixels; end }
   x.report("two_d_array_with_dispatch") { n.times do; ar = img.two_d_array_with_dispatch; end }
+  x.report("two_d_array_with_get_pixels") { n.times do; ar = img.two_d_array_with_get_pixels; end }
 end
 
 color = img(File.dirname(__FILE__) + "/../fixtures/color.jpg")
@@ -30,13 +31,6 @@ Benchmark.bm do |x|
   x.report("manipulate_pixels_with_export_pixels") {
     color = img(File.dirname(__FILE__) + "/../fixtures/color.jpg")
     color.manipulate_pixels_with_export_pixels(locations_array) do |pixel_array|
-      gray_val = (0.3 * pixel_array[0] + 0.59 * pixel_array[1] + 0.11 * pixel_array[2])
-      [gray_val, gray_val, gray_val]
-    end
-  }
-  x.report("manipulate_pixels_with_export_pixels_as_str") {
-    color = img(File.dirname(__FILE__) + "/../fixtures/color.jpg")
-    color.manipulate_pixels_with_export_pixels_as_str(locations_array) do |pixel_array|
       gray_val = (0.3 * pixel_array[0] + 0.59 * pixel_array[1] + 0.11 * pixel_array[2])
       [gray_val, gray_val, gray_val]
     end
@@ -67,6 +61,12 @@ Benchmark.bm do |x|
     n.times {
       color = Magick::ImageList.new(File.dirname(__FILE__) + "/../fixtures/color.jpg")
       color.get_pixels(0,0,color.columns,color.rows)
+    }
+  }
+  x.report("dispatch") {
+    n.times {
+      color = Magick::ImageList.new(File.dirname(__FILE__) + "/../fixtures/color.jpg")
+      color.dispatch(0,0,color.columns,color.rows,"RGB")
     }
   }
   x.report("view") {
