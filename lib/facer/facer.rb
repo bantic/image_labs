c88d846ab43fb340c5024dc8e602946a9ee159bc
Cory <cory.forsyth@gmail.com>
2009-03-22 22:19:27 -0400

    updated facer lib

--- lib/facer/facer.rb
+++ lib/facer/facer.rb
@@ -1,10 +1,13 @@
 class Facer
   def self.face_detector
-    @@face_detector ||= OpenCV::CvHaarClassifierCascade::load(File.dirname(__FILE__) + "/../../fixtures/haarcascade_frontalface_alt.xml")
+    @@face_detector ||= 
+      OpenCV::CvHaarClassifierCascade::load(
+        File.dirname(__FILE__) + "/../../fixtures/haarcascade_frontalface_alt.xml"
+      )
   end
   
   def self.put_funny_hat_on_celebrity(celebrity_img)
-    r_img = celebrity_img
+    rm_img = celebrity_img
     cv_img = OpenCV::IplImage.load(r_img.filename)
     
     detector = self.face_detector
@@ -14,15 +17,16 @@ class Facer
       rect_w = rect.top_right.x - rect.top_left.x
       rect_h = rect.bottom_left.y  - rect.top_left.y
       
-      hat = img(File.dirname(__FILE__) + "/../../fixtures/party-hat.png")
-      hat = hat.resize(rect_w.to_f * 0.8, rect_h.to_f * 0.8)
+      hat = self.funny_hat.resize(rect_w.to_f * 0.8, rect_h.to_f * 0.8)
       
       x_offset = rect.top_left.x - (hat.columns.to_f * 0.1)
       y_offset = rect.top_left.y - (hat.rows.to_f * 0.95)
-      r_img.composite!(hat, x_offset, y_offset, Magick::OverCompositeOp)
+      rm_img.composite!(hat, x_offset, y_offset, Magick::OverCompositeOp)
     end
-    r_img
+    rm_img
   end
   
-  
+  def self.funny_hat
+    @hat ||= img(File.dirname(__FILE__) + "/../../fixtures/party-hat.png")
+  end
 end
